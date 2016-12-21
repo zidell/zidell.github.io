47a4d0f06b56f5b840a47a37d761d3d8ff932341
zidell <zidell@gmail.com>
2016-12-21 14:18:10 +0900

    u

--- bk/app.js
+++
@@ -1,6 +0,0 @@
-class Test extends React.Component {
-	render() {
-		return (<div><h1>haha bK!!</h1><img src="example.png"/></div>);
-	}
-}
-ReactDOM.render(<Test />, document.getElementById('root'));
