07e24512b29a7494b28e332b8705ac391f5b8ca4
zidell <zidell@gmail.com>
2017-01-06 22:17:23 +0900

    removed

--- coffee-planet/app.js
+++
@@ -1,6 +0,0 @@
-class Test extends React.Component {
-	render() {
-		return (<div><h1>테스트 토이 프로젝트 시작!!</h1><img src="img.png" /></div>);
-	}
-}
-ReactDOM.render(<Test />, document.getElementById('root'));
