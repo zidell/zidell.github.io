95223f026ecc939aff88565b5efb43b6c0653668
zidell <zidell@MacBook.local>
2018-10-12 21:43:24 +0900

    cra test

---
+++ cra-test/precache-manifest.39859174111a3cd5c1b093cc5e5226de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ca2ecf4d6acb763e91e",
+    "url": "/static/js/main.4ca2ecf4.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "4ca2ecf4d6acb763e91e",
+    "url": "/static/css/main.3e7e5979.chunk.css"
+  },
+  {
+    "revision": "461c09495c02349ebf3a79ec37ff7da1",
+    "url": "/index.html"
+  }
+];
