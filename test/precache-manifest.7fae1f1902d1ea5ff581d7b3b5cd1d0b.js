b8aa72d03f21c8ec8bc3ea4f5e61b39a59a6b0ed
zidell <zidell@gmail.com>
2019-03-26 22:49:49 +0900

    Updates

---
+++ test/precache-manifest.7fae1f1902d1ea5ff581d7b3b5cd1d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "753e715467f9b65ed61b",
+    "url": "/test/static/js/main.d7fb1e57.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/test/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "753e715467f9b65ed61b",
+    "url": "/test/static/css/main.20bd48b6.chunk.css"
+  },
+  {
+    "revision": "0480bf1d7e9cdfb24ff2a8650d308208",
+    "url": "/test/index.html"
+  }
+];
